8da33b7fc7db224a0461c71e0bbdedf5bf235e40
sonira <sonira.shokohyan531@gmail.com>
2023-07-16 01:17:00 +0500

    created a new function

--- fourth.py
+++ fourth.py
@@ -1,1 +1,9 @@
 print("Hello this is my 4th python file ")
+
+def marks():
+    math = 100
+    history = 99
+    chemical = 89
+    physics = 99
+    print(math + history + chemical+ physics)
+marks()
